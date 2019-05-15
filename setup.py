6fe884a1c8cd57832adc3cc5ae0ac7c48bba1ff1
Christoph Möhl <cmohl@yahoo.com>
2019-05-15 14:33:41 +0200

    deploy test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='bigtiff',
-    version='0.1.1',
+    version='0.1.2',
     description='A library for random access for large (Big-)TIFF files',
     author='Manuel Schölling',
     author_email='manuel.schoelling@dzne.de',
